bc693522901c0b83f77a3d9e0b3b654c4e021f69
Роман Яцун < >
2016-11-25 00:44:41 +0200

    TESTING

--- dist/sc-router.js
+++ dist/sc-router.js
@@ -39,15 +39,9 @@ var SCRouter = function (_HTMLElement) {
     value: function _onChanged() {
       var _this2 = this;
 
-      var path = window.location.hash.slice(1);
+      var path = window.location.pathname;
       var routes = Array.from(this._routes.keys());
       var route = routes.find(function (r) {
-        console.log(r);
-        console.log("-------");
-        console.log(path);
-        console.log("-------");
-        console.dir(r.test(path));
-        console.log("===========");
         return r.test(path);
       });
       var data = route.exec(path);
